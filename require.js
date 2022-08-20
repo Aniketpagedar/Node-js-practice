34a520ed14eb699b1c143f75722331236ca6240c
Aniket pagedar <81167148+Aniketpagedar@users.noreply.github.com>
2022-08-20 18:25:50 +0530

    It is calling text function example for node js

---
+++ require.js
@@ -0,0 +1,3 @@
+ const fs = require("fs");
+ //fs.writeFileSync('read.txt',"Welcome to Vs code");
+ fs.writeFileSync("")
---
+++ writefilesync.js
@@ -0,0 +1,4 @@
+ const fs = require("fs");
+ //fs.writeFileSync('read.txt',"Welcome to Vs code");
+// fs.writeFileSync('read.txt',"Hello this  is Aniket on vs code")
+  fs.appendFileSync('read.txt',"Hello this  Ani how are you")
